b26932eb77a8f08b9ea8d56a7edae91fe6bd298d
Je9017962 <je901762@ucf.edu>
2026-01-19 10:46:42 -0500

    updated repo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-list/"
+  base: '/Je9017962/todo-list/',   // <-- your repo name here
 })
